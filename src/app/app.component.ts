a70d626b9527cee654121bc76ac87058da47705d
Jamie Peabody <jpeabody@axway.com>
2018-08-06 12:51:19 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+  ]
 })
 export class AppComponent {
   title = 'mergely-angular';
+
+  ngOnInit() {
+    jQuery(document).ready(() => {
+      jQuery('#mergely').mergely({
+        lhs: (setValue) => {
+          setValue('the quick red fox\njumped over the hairy dog');
+        },
+        rhs: (setValue) => {
+          setValue('the quick brown fox\njumped over the lazy dog');
+        }
+      });
+    });
+  }
 }
---
+++ src/typings.d.ts
@@ -0,0 +1,4 @@
+
+interface JQuery {
+   mergely(options?: any) : any;
+}
